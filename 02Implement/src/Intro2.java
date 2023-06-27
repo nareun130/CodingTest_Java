ab989f36d9a2b9d68fd93660c92bd5a4cd713620
nareun130 <nareun130@gmail.com>
2023-06-27 21:35:36 +0900

    [FEAT] : 시각
    완전 탐색 유형으로 분류
    완전 탐색 : 가능한 경우의 수를 모두 검사

---
+++ 02Implement/src/Intro2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Intro2 {
+	/*
+	 * 시각 00시 00분 00초부터 N시 59분 59초까지의 모든 시각 중에서 3이 하나라도 포함되는 모든 경우의 수를 구한다. ex) 00시
+	 * 00분 03초 00시 13분 03초
+	 * 
+	 * 입력초전 N(0~23)정수
+	 * 
+	 * 출력조건 3이 하나라도 포함되는 모든 경우의 수 출력
+	 * 
+	 * N = 5 -> 11475
+	 */
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// N입력 받기
+		int n = sc.nextInt();
+		int count = 0;
+
+		for (int i = 0; i <= n; i++) {
+			for (int j = 0; j < 60; j++) {
+				for (int k = 0; k < 60; k++) {
+					if (check(i, j, k))
+						count++;
+				}
+			}
+		}
+		System.out.println(count);
+		sc.close();
+	}
+
+	private static boolean check(int h, int m, int s) {
+		if (h % 10 == 3 || m / 10 == 3 || m % 10 == 3 || s / 10 == 3 || s % 10 == 3) {
+			return true;
+		}
+		return false;
+	}
+}
