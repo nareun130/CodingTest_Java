df219e1d362eb5f0bb31ee3662a12dd4c9ef019d
nareun130 <nareun130@gmail.com>
2023-06-27 19:53:07 +0900

    [FEAT] : 그리디 Intro 문제

---
+++ 01Greedy/src/Intro.java
@@ -0,0 +1,22 @@
+
+public class Intro {
+	/*
+	 * 투입된 금액이 1260원일 때,
+	 * 500원, 100원, 50원, 10원짜리 동전이 무한히 존재한다고 가정
+	 * 손님에게 거슬러 줘야 할 돈이 N원일 때 거슬러 줘야 할 동전의 최소 개수
+	 * 단, 거슬러 줘야 할 돈 N은 항상 10의 배수
+	 * */
+	public static void main(String[] args) {
+		int n = 1260;
+		int count = 0;
+
+		int[] coinTypes = { 500, 100, 50, 10 };
+
+		for (int i = 0; i < coinTypes.length; i++) {
+			int coin = coinTypes[i];
+			count += n / coin;
+			n %= coin;
+		}
+		System.out.println(count);
+	}
+}
